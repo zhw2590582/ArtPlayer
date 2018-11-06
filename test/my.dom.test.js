9baaf10f0e2f570b1056fc4deb708ec2d90d7558
Harvey Zack <laozhaochaguan@gmail.com>
2018-11-06 17:28:00 +0800

    remove test

--- test/my.dom.test.js
+++
@@ -1,8 +0,0 @@
-import Artplayer from '../src/index';
-
-test('Insert to DOM', t => {
-  document.body.innerHTML = '<div class="artplayer-app"></div>';
-  const app = new Artplayer({
-    container: '.artplayer-app'
-  });
-});
