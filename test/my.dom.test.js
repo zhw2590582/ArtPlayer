0fee4186a2f60213e0b08f0b50e52805d2907e7d
Harvey Zack <laozhaochaguan@gmail.com>
2018-11-06 17:26:22 +0800

    try add test file

--- test/my.dom.test.js
+++ test/my.dom.test.js
@@ -1,6 +1,6 @@
-import Artplayer from '../index';
+import Artplayer from '../src/index';
 
-test('test', () => {
+test('Insert to DOM', t => {
   document.body.innerHTML = '<div class="artplayer-app"></div>';
   const app = new Artplayer({
     container: '.artplayer-app'
