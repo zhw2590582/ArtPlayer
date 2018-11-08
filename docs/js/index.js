b97e49beadeb9d5f7ec39798d62527bc4d2b353a
Harvey Zack <laozhaochaguan@gmail.com>
2018-11-08 16:12:17 +0800

    update docs

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,39 +1,48 @@
-var controlMap = {};
-function creatCodeMirror(id, value) {
-    var mirror = CodeMirror(document.querySelector(`[data-control="${id}"]`), {
-        lineNumbers: true,
-        mode: 'javascript',
-        matchBrackets: true,
-        value: value.trim()
-    });
-    controlMap[Number(id)] = mirror;
-    return mirror;
+var mirror = CodeMirror(document.querySelector(`.code`), {
+    lineNumbers: true,
+    mode: 'javascript',
+    matchBrackets: true,
+    value: ''
+});;
+
+function creatCodeMirror(value) {
+    mirror.setValue(value.trim());
 }
 
 function initApp(app) {
-    console.log(app);
+    Artplayer.config.video.events.forEach(item => {
+        app.on('video:' + item, event => {
+            console.log('video: ' + event.type);
+        });
+    });
+}
+
+function timeTaken(callback) {
+    console.time('timeTaken');
+    const r = callback();
+    console.timeEnd('timeTaken');
+    return r;
 }
 
 var $error = document.querySelector('.error-tip');
-Array.from(document.querySelectorAll('.run')).forEach(function (item) {
-    item.addEventListener('click', function (e) {
-        try {
-            Artplayer.instances.forEach(ins => {
-                ins.destroy(true)
-            });;
-            var id = Number(e.target.dataset.id);
-            eval(controlMap[id].getValue());
-            initApp(Artplayer.instances[0]);
-            $error.innerHTML = '';
-        } catch (error) {
-            var msg = error.message.trim();
-            $error.innerHTML = '请打开控制台：' + msg;
-            console.error(msg);
-        }
-    });
+document.querySelector('.run').addEventListener('click', function (e) {
+    try {
+        Artplayer.instances.forEach(ins => {
+            ins.destroy(true)
+        });;
+        timeTaken(function () {
+            eval(mirror.getValue());
+        });
+        initApp(Artplayer.instances[0]);
+        $error.innerHTML = '';
+    } catch (error) {
+        var msg = error.message.trim();
+        $error.innerHTML = '请打开控制台：' + msg;
+        console.error(msg);
+    }
 });
 
-creatCodeMirror(1, `
+creatCodeMirror(`
 new Artplayer({
     container: '.artplayer-app',
     url: 'https://blog.zhw-island.com/assets-cdn/video/one-more-time-one-more-chance-480p.mp4'
