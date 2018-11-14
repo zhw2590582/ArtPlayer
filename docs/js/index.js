17c34330b5bf3994b4134730e76e52b6195bf9a8
Harvey Zack <laozhaochaguan@gmail.com>
2018-11-14 16:19:16 +0800

    docs fix

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,5 +1,5 @@
 var url = 'https://blog.zhw-island.com/assets-cdn';
-var app = new Artplayer({
+var art = new Artplayer({
   container: '.artplayer-app',
   url: url + '/video/one-more-time-one-more-chance-480p.mp4',
   title: '【新海诚动画】『秒速5センチメートル』',
@@ -35,8 +35,8 @@ var app = new Artplayer({
     {
       html: `<img style="width: 100px" src="${url}/image/your-name.png">`,
       click: function () {
-        app.destroy(true);
-        app = new Artplayer({
+        art.destroy(true);
+        art = new Artplayer({
             autoplay: true,
             container: '.artplayer-app',
             url: url + '/video/you-name.mp4'
