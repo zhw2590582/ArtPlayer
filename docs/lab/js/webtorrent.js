32b43ac5db89dbbb07a4addf4d7694ec68ee066b
Harvey Zack <laozhaochaguan@hotmail.com>
2018-11-24 23:28:51 +0800

    添加更多演示

---
+++ docs/lab/js/webtorrent.js
@@ -0,0 +1,16 @@
+var art = new Artplayer({
+    container: '.artplayer-app',
+    url: 'magnet:?xt=urn:btih:6a9759bffd5c0af65319979fb7832189f4f3c35d&dn=sintel.mp4&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fsintel-1024-surround.mp4',
+    type: 'torrent',
+    customType: {
+        torrent: function (video, url) {
+            var client = new WebTorrent();
+            client.add(url, function (torrent) {
+                var file = torrent.files[0];
+                file.renderTo(video, {
+                    autoplay: true
+                })
+            })
+        },
+    },
+});
