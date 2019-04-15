904f7f6e93a9c5622454b47c96c42b5d3aee3c86
赵海伟 <zhaohaiwei@zhaohaiweis-MacBook-Pro.local>
2019-04-15 20:37:24 +0800

    1.0.8

--- packages/artplayer-tool-thumbnail/dist/artplayer-tool-thumbnail.js
+++ packages/artplayer-tool-thumbnail/dist/artplayer-tool-thumbnail.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-tool-thumbnail.js v1.0.6
+ * artplayer-tool-thumbnail.js v1.0.7
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2019 Harvey Zack
  * Released under the MIT License.
