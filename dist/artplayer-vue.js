f859640be2b934c31f106efee012d790914c2087
Harvey Zack <laozhaochaguan@gmail.com>
2019-04-16 11:33:00 +0800

    build all packages

--- dist/artplayer-vue.js
+++ dist/artplayer-vue.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-vue.js v3.0.0
+ * artplayer-vue.js v3.1.0
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2019 Harvey Zack
  * Released under the MIT License.
