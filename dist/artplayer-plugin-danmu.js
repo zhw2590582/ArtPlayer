9d2e1d1a6d14b7996319f17e16283cef1e978793
Harvey Zack <laozhaochaguan@gmail.com>
2019-04-16 11:35:11 +0800

    build all packages

--- dist/artplayer-plugin-danmu.js
+++
@@ -1,8 +0,0 @@
-/*!
- * artplayer-plugin-danmu.js v3.1.1
- * Github: https://github.com/zhw2590582/ArtPlayer#readme
- * (c) 2017-2019 Harvey Zack
- * Released under the MIT License.
- */
-
-!function(e,n){"object"==typeof exports&&"undefined"!=typeof module?module.exports=n():"function"==typeof define&&define.amd?define(n):(e=e||self)["artplayer-plugin-danmu"]=n()}(this,function(){"use strict";function e(e){var n=e.prototype;console.log(n)}return window.artplayerPluginDanmu=e});
