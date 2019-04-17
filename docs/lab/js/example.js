bb2d8718263142a4155d60553628f25e6e24f45a
Harvey Zack <laozhaochaguan@gmail.com>
2019-04-17 20:50:45 +0800

    Update example.js

--- docs/lab/js/example.js
+++ docs/lab/js/example.js
@@ -1,4 +1,4 @@
-var url = 'https://blog.zhw-island.com/assets-cdn';
+var url = 'https://zhw2590582.github.io/assets-cdn';
 var art = new Artplayer({
     container: '.artplayer-app',
     url: url + '/video/one-more-time-one-more-chance-480p.mp4',
