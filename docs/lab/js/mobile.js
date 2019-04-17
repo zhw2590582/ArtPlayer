7c86519e460b07d62b0663da7cb02b8f529246e2
Harvey Zack <laozhaochaguan@gmail.com>
2019-04-17 20:47:59 +0800

    Update mobile.js

--- docs/lab/js/mobile.js
+++ docs/lab/js/mobile.js
@@ -1,5 +1,5 @@
 var vConsole = new VConsole();
-var url = 'https://blog.zhw-island.com/assets-cdn';
+var url = 'https://zhw2590582.github.io/assets-cdn';
 var art = new Artplayer({
     container: '.artplayer-app',
     url: url + '/video/one-more-time-one-more-chance-480p.mp4',
