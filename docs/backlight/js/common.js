00c8054a8b2eb357603f1d48688875b1936a495c
Harvey Zack <laozhaochaguan@gmail.com>
2019-04-18 18:10:30 +0800

    update demo url

--- docs/backlight/js/common.js
+++ docs/backlight/js/common.js
@@ -3,7 +3,7 @@
 (function() {
     var art = new Artplayer({
         container: '.artplayer',
-        url: 'https://zhw2590582.github.io/assets-cdn/video/one-more-time-one-more-chance-480p.mp4',
+        url: 'https://zhw2590582.github.io/assets-cdn/video/ambilight.mp4',
         autoSize: true,
         moreVideoAttr: {
             crossOrigin: 'anonymous'
