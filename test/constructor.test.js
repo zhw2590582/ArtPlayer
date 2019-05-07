cbde597614be079d4993d63dc675d3bf28d661f5
Harvey Zack <laozhaochaguan@gmail.com>
2019-05-07 17:30:03 +0800

    Update constructor.test.js

--- test/constructor.test.js
+++ test/constructor.test.js
@@ -27,7 +27,7 @@ describe('Constructor', function() {
                     container: null,
                 }),
             );
-        }).to.throw(/option.container require \'string\' or \'Element\' type, but got \'null\'/);
+        }).to.throw(/option.container require \'string\' or \'Element\' type'/);
     });
 
     it('Destroy all instances', function() {
