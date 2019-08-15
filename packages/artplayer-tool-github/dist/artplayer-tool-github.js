2560cee9373492701f83e6a4d038f9b4da477101
Harvey <laozhaochaguan@gmail.com>
2019-08-15 08:34:33 +0800

    build all packages

--- packages/artplayer-tool-github/dist/artplayer-tool-github.js
+++ packages/artplayer-tool-github/dist/artplayer-tool-github.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-tool-github.js v3.1.12
+ * artplayer-tool-github.js v3.1.13
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2019 Harvey Zack
  * Released under the MIT License.
