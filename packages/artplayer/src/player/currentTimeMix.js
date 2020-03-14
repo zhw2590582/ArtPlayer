bb92f3dc3dbc3ed8f148e4c6ee2bacdbbd688670
zhw2590582 <laozhaochaguan@gmail.com>
2020-03-15 07:43:15 +0800

    Update currentTimeMix.js

--- packages/artplayer/src/player/currentTimeMix.js
+++ packages/artplayer/src/player/currentTimeMix.js
@@ -1,10 +1,12 @@
 import { clamp, def } from '../utils';
 
 export default function currentTimeMix(art, player) {
+    const { $video } = art.template;
+
     def(player, 'currentTime', {
-        get: () => art.template.$video.currentTime || 0,
-        set: currentTime => {
-            art.template.$video.currentTime = clamp(currentTime, 0, player.duration);
+        get: () => $video.currentTime || 0,
+        set: time => {
+            $video.currentTime = clamp(time, 0, player.duration || Infinity);
         },
     });
 }
