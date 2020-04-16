51bde726aece5dc52145349a9d5eb82e645b750c
Harvey <laozhaochaguan@gmail.com>
2020-04-16 11:38:19 +0800

    优化blur事件

--- packages/artplayer/src/events/clickInit.js
+++ packages/artplayer/src/events/clickInit.js
@@ -1,10 +1,10 @@
 export default function clickInit(art, events) {
     const { $player } = art.template;
-    events.proxy(document, ['click', 'contextmenu'], event => {
+    events.proxy(document, ['click', 'contextmenu'], (event) => {
         if (event.composedPath && event.composedPath().indexOf($player) > -1) {
             art.isFocus = true;
             art.emit('focus');
-        } else {
+        } else if (art.isFocus) {
             art.isFocus = false;
             art.emit('blur');
         }
