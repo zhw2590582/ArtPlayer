df424ec3ac373814bd3d0086d09f13779448bfb6
Harvey <laozhaochaguan@gmail.com>
2020-04-16 12:18:26 +0800

    优化focus事件

--- packages/artplayer/src/events/clickInit.js
+++ packages/artplayer/src/events/clickInit.js
@@ -2,8 +2,11 @@ export default function clickInit(art, events) {
     const { $player } = art.template;
     events.proxy(document, ['click', 'contextmenu'], (event) => {
         if (event.composedPath && event.composedPath().indexOf($player) > -1) {
-            art.isFocus = true;
-            art.emit('focus');
+            if (!art.isFocus) {
+                art.isFocus = true;
+                art.emit('focus');
+            }
+            art.emit(event.type, event);
         } else if (art.isFocus) {
             art.isFocus = false;
             art.emit('blur');
