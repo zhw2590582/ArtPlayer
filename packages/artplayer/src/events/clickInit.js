ac4e5a9c948518eb90f19c301afe689de771c7f7
Harvey <laozhaochaguan@gmail.com>
2020-04-16 14:57:33 +0800

    Revert "优化focus事件"
    This reverts commit df424ec3ac373814bd3d0086d09f13779448bfb6.

--- packages/artplayer/src/events/clickInit.js
+++ packages/artplayer/src/events/clickInit.js
@@ -2,11 +2,8 @@ export default function clickInit(art, events) {
     const { $player } = art.template;
     events.proxy(document, ['click', 'contextmenu'], (event) => {
         if (event.composedPath && event.composedPath().indexOf($player) > -1) {
-            if (!art.isFocus) {
-                art.isFocus = true;
-                art.emit('focus');
-            }
-            art.emit(event.type, event);
+            art.isFocus = true;
+            art.emit('focus');
         } else if (art.isFocus) {
             art.isFocus = false;
             art.emit('blur');
