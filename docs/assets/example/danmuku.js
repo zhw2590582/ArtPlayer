7f3b11685142318bea737e10519c0de6688d9c56
Harvey <laozhaochaguan@gmail.com>
2020-04-26 08:59:30 +0800

    Update danmuku.js

--- docs/assets/example/danmuku.js
+++ docs/assets/example/danmuku.js
@@ -6,13 +6,46 @@ var art = new Artplayer({
     setting: true,
     playbackRate: true,
     fullscreenWeb: true,
-    plugins: [artplayerPluginDanmuku({
-        danmuku: url + '/danmuku/your-name.xml',
-        speed: 5,
-        maxlength: 50,
-        margin: [10, 100],
-        opacity: 1,
-        fontSize: 25,
-        synchronousPlayback: false,
-    })],
+    plugins: [
+        artplayerPluginDanmuku({
+            danmuku: url + '/danmuku/your-name.xml',
+            speed: 5,
+            maxlength: 50,
+            margin: [10, 100],
+            opacity: 1,
+            fontSize: 25,
+            synchronousPlayback: false,
+        }),
+    ],
+    controls: [
+        {
+            position: 'right',
+            html: 'Hide',
+            click: function () {
+                art.plugins.artplayerPluginDanmuku.hide();
+            },
+        },
+        {
+            position: 'right',
+            html: 'Show',
+            click: function () {
+                art.plugins.artplayerPluginDanmuku.show();
+            },
+        },
+        {
+            position: 'right',
+            html: 'Send',
+            click: function () {
+                var text = prompt('Please enter', 'Danmu text');
+                if (!text || !text.trim()) return;
+                var color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+                art.plugins.artplayerPluginDanmuku.emit({
+                    text: text,
+                    color: color,
+                    border: color,
+                    time: art.player.currentTime + 0.2,
+                });
+            },
+        },
+    ],
 });
