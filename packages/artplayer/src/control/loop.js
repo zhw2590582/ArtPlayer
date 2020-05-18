de5ac7513e7a7f33431c4e2169f06b0eba160262
Harvey <laozhaochaguan@gmail.com>
2020-05-18 16:25:24 +0800

    改进区间循环UI

--- packages/artplayer/src/control/loop.js
+++ packages/artplayer/src/control/loop.js
@@ -6,12 +6,12 @@ export default function loop(option) {
         mounted: ($control) => {
             const { player } = art;
 
-            const $left = append($control, '<span class="art-loop-left"></span>');
-            const $right = append($control, '<span class="art-loop-right"></span>');
+            const $left = append($control, `<span class="art-loop-point"></span>`);
+            const $right = append($control, `<span class="art-loop-point"></span>`);
 
             art.on('loopAdd', (value) => {
                 setStyle($control, 'display', 'block');
-                setStyle($left, 'left', `calc(${(value[0] / player.duration) * 100}% - 6px)`);
+                setStyle($left, 'left', `calc(${(value[0] / player.duration) * 100}% - ${$left.clientWidth}px)`);
                 setStyle($right, 'left', `${(value[1] / player.duration) * 100}%`);
             });
 
