9115e52b52a36da4022d2abb8224796200de2d72
zhw2590582 <laozhaochaguan@gmail.com>
2020-09-02 22:03:22 +0800

    bugfix

--- packages/artplayer/src/control/quality.js
+++ packages/artplayer/src/control/quality.js
@@ -4,7 +4,7 @@ export default function quality(option) {
         const qualityDefault = qualityOption.find((item) => item.default) || qualityOption[0];
         return {
             ...option,
-            html: qualityDefault.name,
+            html: qualityDefault ? qualityDefault.name : '',
             selector: qualityOption,
             onSelect(item) {
                 art.player.switchQuality(item.url, item.name);
