6b9e0b3c42b4797287a930764a206a98803ed592
Harvey <laozhaochaguan@gmail.com>
2020-09-23 15:01:06 +0800

    build all packages

--- dist/artplayer-plugin-gif.js
+++ dist/artplayer-plugin-gif.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-plugin-gif.js v3.5.14
+ * artplayer-plugin-gif.js v3.5.15
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2020 Harvey Zack
  * Released under the MIT License.
