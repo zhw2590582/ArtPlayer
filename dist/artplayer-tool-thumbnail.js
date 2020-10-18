3413cf82a5f4dbc706f59d53c487312c8063a1b8
zhw2590582 <laozhaochaguan@gmail.com>
2020-10-18 14:48:34 +0800

    build all packages

--- dist/artplayer-tool-thumbnail.js
+++ dist/artplayer-tool-thumbnail.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-tool-thumbnail.js v3.5.15
+ * artplayer-tool-thumbnail.js v3.5.16
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2020 Harvey Zack
  * Released under the MIT License.
