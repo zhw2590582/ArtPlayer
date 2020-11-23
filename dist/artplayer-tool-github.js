39819c5b46592c32cb327ae8c20b03bcc929703a
zhw2590582 <laozhaochaguan@gmail.com>
2020-11-23 20:28:39 +0800

    build all packages

--- dist/artplayer-tool-github.js
+++ dist/artplayer-tool-github.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-tool-github.js v3.5.21
+ * artplayer-tool-github.js v3.5.22
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2020 Harvey Zack
  * Released under the MIT License.
