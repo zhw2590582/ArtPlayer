b8c7e7b48b188b31be7db141e0377dd35b2f4bf5
zhw2590582 <laozhaochaguan@gmail.com>
2020-11-23 20:28:04 +0800

    Update danmuku.js

--- packages/artplayer-plugin-danmuku/src/danmuku.js
+++ packages/artplayer-plugin-danmuku/src/danmuku.js
@@ -55,15 +55,18 @@ export default class Danmuku {
     load() {
         if (typeof this.option.danmuku === 'function') {
             this.option.danmuku().then((danmus) => {
+                this.queue = [];
                 danmus.forEach(this.emit.bind(this));
                 this.art.emit('artplayerPluginDanmuku:loaded');
             });
         } else if (typeof this.option.danmuku === 'string') {
             bilibiliDanmuParseFromUrl(this.option.danmuku).then((danmus) => {
+                this.queue = [];
                 danmus.forEach(this.emit.bind(this));
                 this.art.emit('artplayerPluginDanmuku:loaded');
             });
         } else {
+            this.queue = [];
             this.option.danmuku.forEach(this.emit.bind(this));
             this.art.emit('artplayerPluginDanmuku:loaded');
         }
