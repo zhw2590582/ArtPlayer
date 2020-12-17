36d6bf942276d86e104c6eea490613be4090f955
Harvey <laozhaochaguan@gmail.com>
2020-12-17 15:34:19 +0800

    build all packages

--- packages/artplayer-plugin-backlight/dist/artplayer-plugin-backlight.js
+++ packages/artplayer-plugin-backlight/dist/artplayer-plugin-backlight.js
@@ -1,5 +1,5 @@
 /*!
- * artplayer-plugin-backlight.js v3.5.26
+ * artplayer-plugin-backlight.js v3.5.27
  * Github: https://github.com/zhw2590582/ArtPlayer#readme
  * (c) 2017-2020 Harvey Zack
  * Released under the MIT License.
