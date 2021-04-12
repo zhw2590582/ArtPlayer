5cdf65d32f19bffc9703846a79fa6ecb5ec03195
Harvey <laozhaochaguan@gmail.com>
2021-04-12 15:28:41 +0800

    Update durationMix.js

--- packages/artplayer/src/player/durationMix.js
+++ packages/artplayer/src/player/durationMix.js
@@ -2,6 +2,10 @@ import { def } from '../utils';
 
 export default function durationMix(art, player) {
     def(player, 'duration', {
-        get: () => art.template.$video.duration || 0,
+        get: () => {
+            const { duration } = art.template.$video;
+            if (duration === Infinity) return 0;
+            return duration || 0;
+        },
     });
 }
