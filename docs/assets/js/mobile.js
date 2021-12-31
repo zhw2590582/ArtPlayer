d6ea0b235530dc92c7410da043c53182352ea0da
Harvey <laozhaochaguan@gmail.com>
2021-12-31 18:14:30 +0800

    update mobile demo

--- docs/assets/js/mobile.js
+++ docs/assets/js/mobile.js
@@ -58,10 +58,6 @@ var art = new Artplayer({
     whitelist: ['*'],
 });
 
-art.on('ready', function () {
-    art.autoHeight = true;
-});
-
 Artplayer.config.events.forEach(function (item) {
     art.on('video:' + item, function (event) {
         console.log('video: ' + event.type);
