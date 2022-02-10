946ca45202b525dfbfe24ca3023d1d79fd289068
Harvey <laozhaochaguan@gmail.com>
2022-02-10 17:17:03 +0800

    update

--- packages/artplayer/src/setting/aspectRatio.js
+++ packages/artplayer/src/setting/aspectRatio.js
@@ -1,8 +1,10 @@
 export default function aspectRatio(art) {
-    const { i18n } = art;
+    const { i18n, icons } = art;
+    
     return {
         width: 150,
         html: i18n.get('Aspect ratio'),
+        icon: '',
         items: ['default', '4:3', '16:9'].map((item) => {
             return {
                 html: item === 'default' ? i18n.get('Default') : item,
