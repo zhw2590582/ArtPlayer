4a9d98ac4dc9e1d6eee0a1d23f00bc0d25af3125
Harvey <laozhaochaguan@gmail.com>
2022-04-22 13:50:41 +0800

    Create lock.js

---
+++ packages/artplayer/src/plugins/lock.js
@@ -0,0 +1,33 @@
+export default function lock(art) {
+    const { layers } = art;
+
+    layers.add({
+        name: 'miniProgressBar',
+        style: {
+            display: 'none',
+            position: 'absolute',
+            left: 0,
+            right: 0,
+            bottom: 0,
+            height: '2px',
+            backgroundColor: 'var(--theme)',
+        },
+        mounted($progressBar) {
+            art.on('control', (value) => {
+                $progressBar.style.display = value ? 'none' : 'block';
+            });
+
+            art.on('destroy', () => {
+                $progressBar.style.display = 'none';
+            });
+
+            art.on('video:timeupdate', () => {
+                $progressBar.style.width = `${art.played * 100}%`;
+            });
+        },
+    });
+
+    return {
+        name: 'lock',
+    };
+}
