15ea16f779a91ba25eb86acf03ca46d6218b61e6
Harvey Zack <laozhaochaguan@gmail.com>
2022-04-25 21:56:47 +0800

    update

--- docs/uncompiled/artplayer.js
+++ docs/uncompiled/artplayer.js
@@ -1433,210 +1433,176 @@
     });
   }
 
-  var screenfull$1 = {exports: {}};
+  /* eslint-disable promise/prefer-await-to-then */
 
-  /*!
-  * screenfull
-  * v5.2.0 - 2021-11-03
-  * (c) Sindre Sorhus; MIT License
-  */
+  const methodMap = [
+  	[
+  		'requestFullscreen',
+  		'exitFullscreen',
+  		'fullscreenElement',
+  		'fullscreenEnabled',
+  		'fullscreenchange',
+  		'fullscreenerror',
+  	],
+  	// New WebKit
+  	[
+  		'webkitRequestFullscreen',
+  		'webkitExitFullscreen',
+  		'webkitFullscreenElement',
+  		'webkitFullscreenEnabled',
+  		'webkitfullscreenchange',
+  		'webkitfullscreenerror',
 
-  (function (module) {
-  (function () {
+  	],
+  	// Old WebKit
+  	[
+  		'webkitRequestFullScreen',
+  		'webkitCancelFullScreen',
+  		'webkitCurrentFullScreenElement',
+  		'webkitCancelFullScreen',
+  		'webkitfullscreenchange',
+  		'webkitfullscreenerror',
 
-  	var document = typeof window !== 'undefined' && typeof window.document !== 'undefined' ? window.document : {};
-  	var isCommonjs = module.exports;
+  	],
+  	[
+  		'mozRequestFullScreen',
+  		'mozCancelFullScreen',
+  		'mozFullScreenElement',
+  		'mozFullScreenEnabled',
+  		'mozfullscreenchange',
+  		'mozfullscreenerror',
+  	],
+  	[
+  		'msRequestFullscreen',
+  		'msExitFullscreen',
+  		'msFullscreenElement',
+  		'msFullscreenEnabled',
+  		'MSFullscreenChange',
+  		'MSFullscreenError',
+  	],
+  ];
 
-  	var fn = (function () {
-  		var val;
+  const nativeAPI = (() => {
+  	const unprefixedMethods = methodMap[0];
+  	const returnValue = {};
 
-  		var fnMap = [
-  			[
-  				'requestFullscreen',
-  				'exitFullscreen',
-  				'fullscreenElement',
-  				'fullscreenEnabled',
-  				'fullscreenchange',
-  				'fullscreenerror'
-  			],
-  			// New WebKit
-  			[
-  				'webkitRequestFullscreen',
-  				'webkitExitFullscreen',
-  				'webkitFullscreenElement',
-  				'webkitFullscreenEnabled',
-  				'webkitfullscreenchange',
-  				'webkitfullscreenerror'
-
-  			],
-  			// Old WebKit
-  			[
-  				'webkitRequestFullScreen',
-  				'webkitCancelFullScreen',
-  				'webkitCurrentFullScreenElement',
-  				'webkitCancelFullScreen',
-  				'webkitfullscreenchange',
-  				'webkitfullscreenerror'
-
-  			],
-  			[
-  				'mozRequestFullScreen',
-  				'mozCancelFullScreen',
-  				'mozFullScreenElement',
-  				'mozFullScreenEnabled',
-  				'mozfullscreenchange',
-  				'mozfullscreenerror'
-  			],
-  			[
-  				'msRequestFullscreen',
-  				'msExitFullscreen',
-  				'msFullscreenElement',
-  				'msFullscreenEnabled',
-  				'MSFullscreenChange',
-  				'MSFullscreenError'
-  			]
-  		];
-
-  		var i = 0;
-  		var l = fnMap.length;
-  		var ret = {};
-
-  		for (; i < l; i++) {
-  			val = fnMap[i];
-  			if (val && val[1] in document) {
-  				for (i = 0; i < val.length; i++) {
-  					ret[fnMap[0][i]] = val[i];
-  				}
-  				return ret;
+  	for (const methodList of methodMap) {
+  		const exitFullscreenMethod = methodList?.[1];
+  		if (exitFullscreenMethod in document) {
+  			for (const [index, method] of methodList.entries()) {
+  				returnValue[unprefixedMethods[index]] = method;
   			}
+
+  			return returnValue;
   		}
-
-  		return false;
-  	})();
-
-  	var eventNameMap = {
-  		change: fn.fullscreenchange,
-  		error: fn.fullscreenerror
-  	};
-
-  	var screenfull = {
-  		request: function (element, options) {
-  			return new Promise(function (resolve, reject) {
-  				var onFullScreenEntered = function () {
-  					this.off('change', onFullScreenEntered);
-  					resolve();
-  				}.bind(this);
-
-  				this.on('change', onFullScreenEntered);
-
-  				element = element || document.documentElement;
-
-  				var returnPromise = element[fn.requestFullscreen](options);
-
-  				if (returnPromise instanceof Promise) {
-  					returnPromise.then(onFullScreenEntered).catch(reject);
-  				}
-  			}.bind(this));
-  		},
-  		exit: function () {
-  			return new Promise(function (resolve, reject) {
-  				if (!this.isFullscreen) {
-  					resolve();
-  					return;
-  				}
-
-  				var onFullScreenExit = function () {
-  					this.off('change', onFullScreenExit);
-  					resolve();
-  				}.bind(this);
-
-  				this.on('change', onFullScreenExit);
-
-  				var returnPromise = document[fn.exitFullscreen]();
-
-  				if (returnPromise instanceof Promise) {
-  					returnPromise.then(onFullScreenExit).catch(reject);
-  				}
-  			}.bind(this));
-  		},
-  		toggle: function (element, options) {
-  			return this.isFullscreen ? this.exit() : this.request(element, options);
-  		},
-  		onchange: function (callback) {
-  			this.on('change', callback);
-  		},
-  		onerror: function (callback) {
-  			this.on('error', callback);
-  		},
-  		on: function (event, callback) {
-  			var eventName = eventNameMap[event];
-  			if (eventName) {
-  				document.addEventListener(eventName, callback, false);
-  			}
-  		},
-  		off: function (event, callback) {
-  			var eventName = eventNameMap[event];
-  			if (eventName) {
-  				document.removeEventListener(eventName, callback, false);
-  			}
-  		},
-  		raw: fn
-  	};
-
-  	if (!fn) {
-  		if (isCommonjs) {
-  			module.exports = {isEnabled: false};
-  		} else {
-  			window.screenfull = {isEnabled: false};
-  		}
-
-  		return;
   	}
 
-  	Object.defineProperties(screenfull, {
-  		isFullscreen: {
-  			get: function () {
-  				return Boolean(document[fn.fullscreenElement]);
-  			}
-  		},
-  		element: {
-  			enumerable: true,
-  			get: function () {
-  				return document[fn.fullscreenElement];
-  			}
-  		},
-  		isEnabled: {
-  			enumerable: true,
-  			get: function () {
-  				// Coerce to boolean in case of old WebKit
-  				return Boolean(document[fn.fullscreenEnabled]);
-  			}
-  		}
-  	});
-
-  	if (isCommonjs) {
-  		module.exports = screenfull;
-  	} else {
-  		window.screenfull = screenfull;
-  	}
+  	return false;
   })();
-  }(screenfull$1));
 
-  var screenfull = screenfull$1.exports;
+  const eventNameMap = {
+  	change: nativeAPI.fullscreenchange,
+  	error: nativeAPI.fullscreenerror,
+  };
+
+  // eslint-disable-next-line import/no-mutable-exports
+  let screenfull = {
+  	// eslint-disable-next-line default-param-last
+  	request(element = document.documentElement, options) {
+  		return new Promise((resolve, reject) => {
+  			const onFullScreenEntered = () => {
+  				screenfull.off('change', onFullScreenEntered);
+  				resolve();
+  			};
+
+  			screenfull.on('change', onFullScreenEntered);
+
+  			const returnPromise = element[nativeAPI.requestFullscreen](options);
+
+  			if (returnPromise instanceof Promise) {
+  				returnPromise.then(onFullScreenEntered).catch(reject);
+  			}
+  		});
+  	},
+  	exit() {
+  		return new Promise((resolve, reject) => {
+  			if (!screenfull.isFullscreen) {
+  				resolve();
+  				return;
+  			}
+
+  			const onFullScreenExit = () => {
+  				screenfull.off('change', onFullScreenExit);
+  				resolve();
+  			};
+
+  			screenfull.on('change', onFullScreenExit);
+
+  			const returnPromise = document[nativeAPI.exitFullscreen]();
+
+  			if (returnPromise instanceof Promise) {
+  				returnPromise.then(onFullScreenExit).catch(reject);
+  			}
+  		});
+  	},
+  	toggle(element, options) {
+  		return screenfull.isFullscreen ? screenfull.exit() : screenfull.request(element, options);
+  	},
+  	onchange(callback) {
+  		screenfull.on('change', callback);
+  	},
+  	onerror(callback) {
+  		screenfull.on('error', callback);
+  	},
+  	on(event, callback) {
+  		const eventName = eventNameMap[event];
+  		if (eventName) {
+  			document.addEventListener(eventName, callback, false);
+  		}
+  	},
+  	off(event, callback) {
+  		const eventName = eventNameMap[event];
+  		if (eventName) {
+  			document.removeEventListener(eventName, callback, false);
+  		}
+  	},
+  	raw: nativeAPI,
+  };
+
+  Object.defineProperties(screenfull, {
+  	isFullscreen: {
+  		get: () => Boolean(document[nativeAPI.fullscreenElement]),
+  	},
+  	element: {
+  		enumerable: true,
+  		get: () => document[nativeAPI.fullscreenElement] ?? undefined,
+  	},
+  	isEnabled: {
+  		enumerable: true,
+  		// Coerce to boolean in case of old WebKit.
+  		get: () => Boolean(document[nativeAPI.fullscreenEnabled]),
+  	},
+  });
+
+  if (!nativeAPI) {
+  	screenfull = {isEnabled: false};
+  }
+
+  var screenfull$1 = screenfull;
 
   var nativeScreenfull = function nativeScreenfull(art) {
     var notice = art.notice,
         $player = art.template.$player;
-    screenfull.on('change', function () {
-      return art.emit('fullscreen', screenfull.isFullscreen);
+    screenfull$1.on('change', function () {
+      return art.emit('fullscreen', screenfull$1.isFullscreen);
     });
     def(art, 'fullscreen', {
       get: function get() {
-        return screenfull.isFullscreen;
+        return screenfull$1.isFullscreen;
       },
       set: function set(value) {
         if (value) {
-          screenfull.request($player).then(function () {
+          screenfull$1.request($player).then(function () {
             addClass($player, 'art-fullscreen');
             art.aspectRatioReset = true;
             art.autoSize = false;
@@ -1645,7 +1611,7 @@
             notice.show = '';
           });
         } else {
-          screenfull.exit().then(function () {
+          screenfull$1.exit().then(function () {
             removeClass($player, 'art-fullscreen');
             art.aspectRatioReset = true;
             art.autoSize = art.option.autoSize;
@@ -1684,7 +1650,7 @@
         notice = art.notice,
         $video = art.template.$video;
     art.once('video:loadedmetadata', function () {
-      if (screenfull.isEnabled) {
+      if (screenfull$1.isEnabled) {
         nativeScreenfull(art);
       } else if (document.fullscreenEnabled || $video.webkitSupportsFullscreen) {
         webkitScreenfull(art);
@@ -4646,9 +4612,18 @@
           addClass($tooltip, 'art-setting-item-right-tooltip');
           append($right, $tooltip);
           item._$tooltip = $tooltip;
+          var tooltip = item.tooltip;
+          def(item, 'tooltip', {
+            get: function get() {
+              return $tooltip.innerHTML;
+            },
+            set: function set(value) {
+              $tooltip.innerHTML = value;
+            }
+          });
 
-          if (item.tooltip) {
-            $tooltip.innerHTML = item.tooltip;
+          if (tooltip) {
+            item.tooltip = tooltip;
           }
 
           var $iconRight = document.createElement('div');
@@ -4689,6 +4664,7 @@
           this.option.push(callback);
         }
 
+        this.cache = new Map();
         this.option = makeRecursion(this.option);
         this.init(this.option);
       }
@@ -5148,7 +5124,7 @@
     }, {
       key: "build",
       get: function get() {
-        return '1650873214309';
+        return '1650894674517';
       }
     }, {
       key: "config",
