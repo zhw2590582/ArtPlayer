26fba8e2a62df77b1c6e507b6ad0c35c91c3cf92
Harvey <laozhaochaguan@gmail.com>
2022-05-05 16:41:01 +0800

    init

---
+++ packages/artplayer-plugin-danmuku/src/control/index.js
@@ -0,0 +1,14 @@
+export default class Control {
+    constructor(danmuku) {
+        this.danmuku = danmuku;
+        this.apps = new Map();
+    }
+
+    mount() {
+        //
+    }
+
+    unmount() {
+        //
+    }
+}
