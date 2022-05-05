a39a64b3b1300c9b024cb607d36004b0c71dd8e8
Harvey <laozhaochaguan@gmail.com>
2022-05-05 16:41:01 +0800

    init

--- packages/artplayer-plugin-danmuku/src/index.js
+++ packages/artplayer-plugin-danmuku/src/index.js
@@ -1,14 +1,19 @@
 import Danmuku from './danmuku';
+import Control from './control';
 
 export default function artplayerPluginDanmuku(option) {
     return (art) => {
         const danmuku = new Danmuku(art, option);
+        const control = new Control(danmuku);
+
         return {
             name: 'artplayerPluginDanmuku',
             emit: danmuku.emit.bind(danmuku),
             config: danmuku.config.bind(danmuku),
             hide: danmuku.hide.bind(danmuku),
             show: danmuku.show.bind(danmuku),
+            mount: control.mount.bind(control),
+            unmount: control.unmount.bind(control),
             get isHide() {
                 return danmuku.isHide;
             },
