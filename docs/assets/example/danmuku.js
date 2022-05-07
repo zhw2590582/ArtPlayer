628cc0633bb6d40f1f259aac4f052174b175b3f0
Harvey Zack <laozhaochaguan@gmail.com>
2022-05-07 23:29:08 +0800

    update sw.js

--- docs/assets/example/danmuku.js
+++ docs/assets/example/danmuku.js
@@ -8,15 +8,13 @@ var art = new Artplayer(
         plugins: [
             artplayerPluginDanmuku({
                 danmuku: '/assets/sample/danmuku-v2.xml',
-                speed: 5,
-                opacity: 1,
-                fontSize: 20,
-                maxlength: 100,
-                margin: ['10%', 60],
-                antiOverlap: true,
-                useWorker: true,
-                synchronousPlayback: false,
-                filter: (danmu) => danmu.text.length < 50,
+                speed: 5, // 弹幕持续时间，单位秒
+                opacity: 1, // 弹幕透明度
+                margin: ['10%', 60], // 弹幕上下边距
+                antiOverlap: true, // 是否防重叠
+                useWorker: true, // 是否使用 web worker
+                synchronousPlayback: false, // 是否同步到播放速度
+                filter: (danmu) => danmu.text.length < 50, // 弹幕过滤函数
             }),
         ],
     },
