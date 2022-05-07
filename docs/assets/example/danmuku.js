68c2c4058b3f98ed82daf72a06e4ffa1e62ed8f8
Harvey Zack <laozhaochaguan@gmail.com>
2022-05-07 23:37:18 +0800

    Update danmuku.js

--- docs/assets/example/danmuku.js
+++ docs/assets/example/danmuku.js
@@ -17,6 +17,35 @@ var art = new Artplayer(
                 filter: (danmu) => danmu.text.length < 50, // 弹幕过滤函数
             }),
         ],
+        controls: [
+            {
+                position: 'right',
+                html: '隐藏',
+                click: function (_, event) {
+                    if (art.plugins.artplayerPluginDanmuku.isHide) {
+                        art.plugins.artplayerPluginDanmuku.show();
+                        event.target.innerText = '隐藏';
+                    } else {
+                        art.plugins.artplayerPluginDanmuku.hide();
+                        event.target.innerText = '显示';
+                    }
+                },
+            },
+            {
+                position: 'right',
+                html: '发送',
+                click: function () {
+                    var text = prompt('请输入弹幕文本', '弹幕测试文本');
+                    if (!text || !text.trim()) return;
+                    var color = '#' + Math.floor(Math.random() * 0xffffff).toString(16);
+                    art.plugins.artplayerPluginDanmuku.emit({
+                        text: text,
+                        color: color,
+                        border: true,
+                    });
+                },
+            },
+        ],
     },
     function () {
         const danmuku = document.querySelector('.artplayer-danmuku');
