8bcc020627540a0a9e5daf75a792acbeb3035d4f
Harvey <laozhaochaguan@gmail.com>
2022-05-14 11:37:56 +0800

    add danmuku colors

--- docs/assets/example/danmuku.js
+++ docs/assets/example/danmuku.js
@@ -1,4 +1,4 @@
-// 使用文档
+// 使用说明
 // https://artplayer.org/document/zh-cn/Plugins/danmuku
 
 var art = new Artplayer({
@@ -17,6 +17,8 @@ var art = new Artplayer({
             speed: 5, // 弹幕持续时间，单位秒，范围在[1 ~ 10]
             opacity: 1, // 弹幕透明度，范围在[0 ~ 1]
             fontSize: 25, // 字体大小，支持数字和百分比
+            color: '#FFFFFF', // 默认字体颜色
+            mode: 0, // 默认模式，0-滚动，1-静止
             margin: ['2%', 60], // 弹幕上下边距，支持数字和百分比
             antiOverlap: true, // 是否防重叠
             useWorker: true, // 是否使用 web worker
@@ -28,5 +30,45 @@ var art = new Artplayer({
 
 // 监听手动输入的弹幕，保存到数据库
 art.on('artplayerPluginDanmuku:emit', (danmu) => {
-    console.info(danmu);
+    console.info('新增弹幕', danmu);
+});
+
+// 监听加载到的弹幕数组
+art.on('artplayerPluginDanmuku:loaded', (danmus) => {
+    console.info('加载弹幕', danmus.length);
+});
+
+// 监听加载到弹幕的错误
+art.on('artplayerPluginDanmuku:error', (error) => {
+    console.info('加载错误', error);
+});
+
+// 监听弹幕配置变化
+art.on('artplayerPluginDanmuku:config', (option) => {
+    console.info('配置变化', option);
+});
+
+// 监听弹幕停止
+art.on('artplayerPluginDanmuku:stop', () => {
+    console.info('弹幕停止');
+});
+
+// 监听弹幕开始
+art.on('artplayerPluginDanmuku:start', () => {
+    console.info('弹幕开始');
+});
+
+// 监听弹幕隐藏
+art.on('artplayerPluginDanmuku:hide', () => {
+    console.info('弹幕隐藏');
+});
+
+// 监听弹幕显示
+art.on('artplayerPluginDanmuku:show', () => {
+    console.info('弹幕显示');
+});
+
+// 监听弹幕销毁
+art.on('artplayerPluginDanmuku:destroy', () => {
+    console.info('弹幕销毁');
 });
