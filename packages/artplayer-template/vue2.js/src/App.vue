107d8842742dd602c637c254f709c472151789ba
Harvey <laozhaochaguan@gmail.com>
2022-06-13 15:00:06 +0800

    add vue2 demo

---
+++ packages/artplayer-template/vue2.js/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <Artplayer @get-instance="getInstance" :option="option" :style="style" />
+</template>
+
+<script>
+import Artplayer from './Artplayer.vue';
+
+export default {
+    data() {
+        return {
+            option: {
+                url: 'https://artplayer.org/assets/sample/video.mp4',
+                autoSize: true,
+            },
+            style: {
+                width: '600px',
+                height: '400px',
+                margin: '60px auto 0',
+            },
+        };
+    },
+    components: {
+        Artplayer,
+    },
+    methods: {
+        getInstance(art) {
+            console.log(art);
+        },
+    },
+};
+</script>
