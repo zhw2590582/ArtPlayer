ec09bcdc92ce311de098c3e3533773a6de36ea16
Harvey <laozhaochaguan@gmail.com>
2022-07-19 14:16:38 +0800

    update types.test.ts

--- test/types.test.ts
+++ test/types.test.ts
@@ -1,5 +1,6 @@
 import Artplayer from '../packages/artplayer';
 import artplayerPluginDanmuku from '../packages/artplayer-plugin-danmuku';
+import artplayerPluginAds from '../packages/artplayer-plugin-ads';
 
 const art = new Artplayer({
     container: '.artplayer-app',
@@ -10,13 +11,26 @@ const art = new Artplayer({
             margin: [0, '10%'],
             theme: 'light',
         }),
+        artplayerPluginAds({
+            html: '<img src="/assets/sample/poster.jpg">',
+            video: '/assets/sample/test1.mp4',
+            url: 'http://artplayer.org',
+        }),
     ],
 });
 
-art.plugins.add(
-    artplayerPluginDanmuku({
-        danmuku: '',
-        margin: [0, '10%'],
-        theme: 'light',
-    }),
-);
+art.plugins
+    .add(
+        artplayerPluginDanmuku({
+            danmuku: '',
+            margin: [0, '10%'],
+            theme: 'light',
+        }),
+    )
+    .add(
+        artplayerPluginAds({
+            html: '<img src="/assets/sample/poster.jpg">',
+            video: '/assets/sample/test1.mp4',
+            url: 'http://artplayer.org',
+        }),
+    );
