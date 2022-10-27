2aa95fd3a8467092c14441bac853aac0791b571a
Harvey <laozhaochaguan@gmail.com>
2022-10-27 16:12:43 +0800

    Update hls.quality.js

--- docs/assets/example/hls.quality.js
+++ docs/assets/example/hls.quality.js
@@ -17,24 +17,10 @@ var art = new Artplayer({
     ],
     customType: {
         m3u8: function (video, url) {
-            if (Hls.isSupported()) {
-                // Destroy the last decoder instance before switching url
-                if (art.hls) art.hls.destroy();
-
-                // When the player is destroyed, the decoder instance must also be destroyed
-                art.on('destroy', () => {
-                    if (art.hls) art.hls.destroy();
-                });
-
-                // Attach the Hls instance to the Artplayer instance
-                art.hls = new Hls();
-                art.hls.loadSource(url);
-                art.hls.attachMedia(video);
-            } else if (video.canPlayType('application/vnd.apple.mpegurl')) {
-                video.src = url;
-            } else {
-                art.notice.show = 'Does not support playback of m3u8';
-            }
+            // Attach the Hls instance to the Artplayer instance
+            art.hls = new Hls();
+            art.hls.loadSource(url);
+            art.hls.attachMedia(video);
         },
     },
 });
