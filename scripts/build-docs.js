21b514d205ea9fa8ee6d1f896ce8b9ac18ddd953
Harvey Zack <laozhaochaguan@gmail.com>
2023-01-11 20:42:43 +0800

    update

--- scripts/build-docs.js
+++ scripts/build-docs.js
@@ -1,1 +1,8 @@
-console.log('build-docs.js');
+import spawn from 'cross-spawn';
+
+const proc = spawn('npm', ['run', 'docs:build'], {
+    cwd: './packages/artplayer-vitepress/',
+});
+
+proc.stdout.pipe(process.stdout);
+proc.stderr.pipe(process.stderr);
