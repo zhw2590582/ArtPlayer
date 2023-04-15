0a0668d908c8d1607a96dcb814770c2f33d947a1
Harvey Zack <laozhaochaguan@gmail.com>
2023-04-15 11:09:55 +0800

    update dep

--- scripts/build-ts.js
+++ scripts/build-ts.js
@@ -1,6 +1,6 @@
 import fs from 'fs';
 import path from 'path';
-import glob from 'glob';
+import { glob } from 'glob';
 
 const reg = /^import.*;$/gim;
 const types = glob.sync('packages/artplayer/types/*');
