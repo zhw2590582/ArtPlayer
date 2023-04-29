17f6d46fe0c74bfe26772f9161c12ec2533f65da
Harvey Zack <laozhaochaguan@gmail.com>
2023-04-29 23:04:12 +0800

    update docs

--- docs/document/main.js
+++ docs/document/main.js
@@ -2,7 +2,7 @@
     if (window['run-code-init']) return;
     window['run-code-init'] = true;
     document.addEventListener('click', (event) => {
-        if (event.target.className === 'run-code') {
+        if (event.target.className === 'run-code' || event.target.getAttribute('classname') === 'run-code') {
             event.preventDefault();
             const codeElement = event.target.nextElementSibling.querySelector('pre code');
             if (codeElement) {
