133de91289f7f81e5dbfb60a44694b7f26d8ae8e
Harvey Zack <laozhaochaguan@gmail.com>
2023-05-01 23:03:09 +0800

    update

--- docs/uncompiled/artplayer-plugin-danmuku/index.js
+++ docs/uncompiled/artplayer-plugin-danmuku/index.js
@@ -1190,10 +1190,32 @@ function heatmap(art, danmuku) {
                 ctx.fillStyle = color;
                 ctx.clearRect(0, 0, $canvas.width, $canvas.height);
                 const gap = art.duration / $canvas.width;
-                for(let index = 1; index <= $canvas.width; index += 10){
+                const points = [];
+                for(let index = 1; index <= $canvas.width; index += 1){
                     const length = danmuku.danmus.filter((item)=>item.time > (index - 1) * gap && item.time <= index * gap).length + 5;
-                    ctx.fillRect(index - 1, height - length, 1, length);
+                    points.push({
+                        x: index - 1,
+                        y: height - length
+                    });
                 }
+                function drawCurve(points, tension) {
+                    ctx.beginPath();
+                    ctx.moveTo(points[0].x, points[0].y);
+                    var t = tension != null ? tension : 1;
+                    for(var i = 0; i < points.length - 1; i++){
+                        var p0 = i > 0 ? points[i - 1] : points[0];
+                        var p1 = points[i];
+                        var p2 = points[i + 1];
+                        var p3 = i != points.length - 2 ? points[i + 2] : p2;
+                        var cp1x = p1.x + (p2.x - p0.x) / 6 * t;
+                        var cp1y = p1.y + (p2.y - p0.y) / 6 * t;
+                        var cp2x = p2.x - (p3.x - p1.x) / 6 * t;
+                        var cp2y = p2.y - (p3.y - p1.y) / 6 * t;
+                        ctx.bezierCurveTo(cp1x, cp1y, cp2x, cp2y, p2.x, p2.y);
+                    }
+                    ctx.stroke();
+                }
+                drawCurve(points);
             }
             art.on("ready", update);
             art.on("resize", update);
