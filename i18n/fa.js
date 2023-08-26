2fc9d4d3e9cd29a2a609d1934b18134b769bcc5b
Harvey Zack <laozhaochaguan@gmail.com>
2023-08-26 16:48:19 +0800

    Dep (#601)
    * init
    
    * update dep

---
+++ i18n/fa.js
@@ -0,0 +1,44 @@
+const fa = {
+    "Video Info": "اطلاعات ویدیو",
+    "Close": "بستن",
+    "Video Load Failed": "بارگذاری ناموفق",
+    "Play": "پخش",
+    "Volume": "میزان صدا",
+    "Pause": "توقف",
+    "Rate": "نرخ",
+    "Mute": "سکوت",
+    "Video Flip": "چرخش تصویر",
+    "Horizontal": "افقی",
+    "Vertical": "عمودی",
+    "Reconnect": "اتصال مجدد",
+    "Show Setting": "تنظیمات",
+    "Hide Setting": "بستن تنظیمات",
+    "Screenshot": "عکس از صفحه",
+    "Play Speed": "سرعت پخش",
+    "Aspect Ratio": "نسبت تصویر",
+    "Default": "حالت پیشفرض",
+    "Normal": " حالت عادی",
+    "Open": "بازکردن",
+    "Switch Video": "تغییر ویدیو",
+    "Switch Subtitle": "نغییر زیرنویس",
+    "Fullscreen": "تمام صفحه",
+    "Exit Fullscreen": "کوچک کردن",
+    "Web Fullscreen": "حالت تئاتر",
+    "Exit Web Fullscreen": "خروج از حالت تئاتر",
+    "Mini Player": "حالت پخش کوچک",
+    "PIP Mode": " مینی پلیر",
+    "Exit PIP Mode": "خروج از مینی پلیر",
+    "PIP Not Supported": "عدم پشتیبانی از مینی پلیر",
+    "Fullscreen Not Supported": "عدم پشتیبانی از حالت تمام صفحه",
+    "Subtitle Offset": "افست زیرنویس",
+    "Last Seen": "آخرین بازدید",
+    "Jump Play": "جامپ پلی",
+    "AirPlay": "ایر پلی",
+    "AirPlay Not Available": "عدم پشتیبانی از ایرپلی"
+}
+
+export default fa;
+
+if (typeof window !== 'undefined') {
+    window['artplayer-i18n-fa'] = fa;
+}
