b711291aee62ffedc4c00bc4b66552399f6be699
Harvey Zack <laozhaochaguan@gmail.com>
2024-08-21 23:52:47 +0800

    refactor: Update Artplayer's video element and add support for custom canvas or video
    The code changes in this commit update the video element in Artplayer to support custom canvas or video. This enhancement allows for more flexibility in handling video playback and rendering. Additionally, new options such as volume, autoplay, autoSize, screenshot, setting, loop, flip, playbackRate, aspectRatio, fullscreen, fullscreenWeb, miniProgressBar, and autoPlayback are added to provide more control over the video player. These changes improve the functionality and usability of Artplayer.

--- docs/assets/example/webav.js
+++ docs/assets/example/webav.js
@@ -5,6 +5,19 @@
 var art = new Artplayer({
     container: '.artplayer-app',
     url: '/assets/sample/frag_bunny.mp4',
+    volume: 0.5,
+    autoplay: false,
+    autoSize: false,
+    screenshot: true,
+    setting: true,
+    loop: true,
+    flip: true,
+    playbackRate: true,
+    aspectRatio: true,
+    fullscreen: true,
+    fullscreenWeb: true,
+    miniProgressBar: true,
+    autoPlayback: true,
     proxy: artplayerProxyWebAV({
         //
     }),
