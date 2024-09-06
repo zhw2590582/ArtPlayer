afcfebe01ee9572d9e70c493b6b1f18334144015
Harvey Zack <laozhaochaguan@gmail.com>
2024-09-06 10:02:29 +0800

    Update Artplayer.vue

--- packages/artplayer-template/vue3.js/src/Artplayer.vue
+++ packages/artplayer-template/vue3.js/src/Artplayer.vue
@@ -4,7 +4,7 @@
 
 <script setup>
 import Artplayer from 'artplayer';
-import { ref, onMounted, onBeforeUnmount, nextTick } from 'vue';
+import { ref, shallowRef, onMounted, onBeforeUnmount, nextTick } from 'vue';
 
 const emit = defineEmits(['get-instance']);
 
