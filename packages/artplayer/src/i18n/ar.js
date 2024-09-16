b733d28182aa37e2452637c5b0c762653efe679b
Sumer Ahmed <sumer5020@gmail.com>
2024-09-16 17:08:11 +0300

    feat: Arabic language support added

---
+++ packages/artplayer/src/i18n/ar.js
@@ -0,0 +1,44 @@
+const ar = {
+    'Video Info': 'معلومات الفيديو',
+    Close: 'إغلاق',
+    'Video Load Failed': 'فشل تحميل الفيديو',
+    Volume: 'الصوت',
+    Play: 'تشغيل',
+    Pause: 'إيقاف مؤقت',
+    Rate: 'السرعة',
+    Mute: 'كتم الصوت',
+    'Video Flip': 'تدوير الفيديو',
+    Horizontal: 'أفقي',
+    Vertical: 'عمودي',
+    Reconnect: 'إعادة الاتصال',
+    'Show Setting': 'إظهار الإعدادات',
+    'Hide Setting': 'إخفاء الإعدادات',
+    Screenshot: 'لقطة شاشة',
+    'Play Speed': 'سرعة التشغيل',
+    'Aspect Ratio': 'نسبة العرض إلى الارتفاع',
+    Default: 'افتراضي',
+    Normal: 'عادي',
+    Open: 'فتح',
+    'Switch Video': 'تبديل الفيديو',
+    'Switch Subtitle': 'تبديل الترجمة',
+    Fullscreen: 'وضع ملء الشاشة',
+    'Exit Fullscreen': 'الخروج من وضع ملء الشاشة',
+    'Web Fullscreen': 'وضع ملء الشاشة في المتصفح',
+    'Exit Web Fullscreen': 'الخروج من وضع ملء الشاشة في المتصفح',
+    'Mini Player': 'مشغل صغير',
+    'PIP Mode': 'الوضع العائم',
+    'Exit PIP Mode': 'الخروج من الوضع العائم',
+    'PIP Not Supported': 'الوضع العائم غير مدعوم',
+    'Fullscreen Not Supported': 'وضع ملء الشاشة غير مدعوم',
+    'Subtitle Offset': 'إزاحة الترجمة',
+    'Last Seen': 'آخر مشاهدة',
+    'Jump Play': 'التشغيل القافز',
+    AirPlay: 'AirPlay',
+    'AirPlay Not Available': 'AirPlay غير متاح',
+};
+
+export default ar;
+
+if (typeof window !== 'undefined') {
+    window['artplayer-i18n-ar'] = ar;
+}
