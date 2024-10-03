cec05c52aee2cc0390b33a69bc8baf24aa194633
emberantz <emberantz@proton.me>
2024-10-03 17:42:00 +0200

    Update subtitle.js

--- packages/artplayer/src/subtitle.js
+++ packages/artplayer/src/subtitle.js
@@ -135,7 +135,7 @@ export default class Subtitle extends Component {
         this.option = subtitleOption;
         this.style(subtitleOption.style);
 
-        return fetch(subtitleOption.url, { method: "cors" })
+        return fetch(subtitleOption.url, { mode: "cors" })
             .then((response) => response.arrayBuffer())
             .then((buffer) => {
                 const decoder = new TextDecoder(subtitleOption.encoding);
