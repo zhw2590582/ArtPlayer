56aa265e53d63cd485626ae191168613c460cf4b
Harvey Zack <laozhaochaguan@gmail.com>
2025-05-22 12:27:58 +0800

    Update asr.js

--- docs/assets/example/asr.js
+++ docs/assets/example/asr.js
@@ -1,13 +1,47 @@
+var ws = null;
+
 var art = new Artplayer({
     container: '.artplayer-app',
     url: '/assets/sample/video.mp4',
     plugins: [
         artplayerPluginAsr({
-            interval: 1000,
+            interval: 2000,
             sampleRate: 16000,
             onAudioChunk: (buffer) => {
-                console.log('Audio buffer:', buffer);
+                return new Promise(async (resolve) => {
+                    if (!ws) {
+                        const {url} = await (await fetch('http://localhost:9001/asr')).json();
+                        ws = new WebSocket(url);
+                        ws.binaryType = 'arraybuffer';
+                        ws.onopen = () => {
+                            console.log('WebSocket connection opened');
+                        };
+                        ws.onclose = () => {
+                            console.log('WebSocket connection closed');
+                            ws = null;
+                        };
+                        ws.onerror = (error) => {
+                            console.error('WebSocket error:', error);
+                        };
+                        ws.onmessage = (event) => {
+                            resolve(event.data);
+                        };
+                    }
+                    if (ws.readyState === WebSocket.OPEN) {
+                        ws.send(buffer);
+                    } else {
+                        console.error('WebSocket is not open. Ready state:', ws.readyState);
+                    }
+                });
+  
             },
         }),
     ]
+});
+
+art.on('destroy', () => {
+    if (ws) {
+        ws.close();
+        ws = null;
+    }
 });
