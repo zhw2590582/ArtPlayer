50320bad5d21640e6c7c8a681f3f74dfbb0e6fa6
Harvey Zack <laozhaochaguan@gmail.com>
2025-08-17 18:25:52 +0800

    feat: add module and exports fields in package.json; improve style handling in index.js

--- scripts/plugin/template/src/index.js
+++ scripts/plugin/template/src/index.js
@@ -10,15 +10,20 @@ export default function {{export}}(option) {
 
 if (typeof document !== 'undefined') {
     const id = 'artplayer-plugin-{{name}}';
-    const $style = document.getElementById(id);
-    if ($style) {
-        $style.textContent = style;
-    } else {
-        const $style = document.createElement('style');
-        $style.id = id;
-        $style.textContent = style;
-        document.head.appendChild($style);
+    let $style = document.getElementById(id)
+    if (!$style) {
+        $style = document.createElement('style')
+        $style.id = id
+        if (document.readyState === 'loading') {
+        document.addEventListener('DOMContentLoaded', () => {
+            document.head.appendChild($style)
+        })
+        }
+        else {
+        (document.head || document.documentElement).appendChild($style)
+        }
     }
+    $style.textContent = style
 }
 
 if (typeof window !== 'undefined') {
