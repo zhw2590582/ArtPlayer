241b4f3ee3e5de0d74b5d993c2c8aaeb5cbcc988
Harvey Zack <laozhaochaguan@gmail.com>
2025-08-17 18:13:14 +0800

    chore: update .npmignore files to include yarn.lock and package-lock.json

--- packages/artplayer-plugin-iframe/test/type.test.ts
+++
@@ -1,6 +0,0 @@
-import ArtplayerPluginIframe from '..';
-
-const iframe = new ArtplayerPluginIframe({
-    iframe: document.getElementById('iframe') as HTMLIFrameElement,
-    url: 'iframe.html',
-});
