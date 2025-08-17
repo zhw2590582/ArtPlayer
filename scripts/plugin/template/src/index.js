79358663ebf932ad1accabe046c600fd76e1119e
Harvey Zack <laozhaochaguan@gmail.com>
2025-08-17 18:43:57 +0800

    fix: improve indentation and formatting for style element creation in plugin

--- scripts/plugin/template/src/index.js
+++ scripts/plugin/template/src/index.js
@@ -9,21 +9,21 @@ export default function {{export}}(option) {
 }
 
 if (typeof document !== 'undefined') {
-    const id = 'artplayer-plugin-{{name}}';
-    let $style = document.getElementById(id)
-    if (!$style) {
-        $style = document.createElement('style')
-        $style.id = id
-        if (document.readyState === 'loading') {
-        document.addEventListener('DOMContentLoaded', () => {
-            document.head.appendChild($style)
-        })
-        }
-        else {
-        (document.head || document.documentElement).appendChild($style)
-        }
+  const id = 'artplayer-plugin-{{name}}';
+  let $style = document.getElementById(id)
+  if (!$style) {
+    $style = document.createElement('style')
+    $style.id = id
+    if (document.readyState === 'loading') {
+      document.addEventListener('DOMContentLoaded', () => {
+        document.head.appendChild($style)
+      })
     }
-    $style.textContent = style
+    else {
+      (document.head || document.documentElement).appendChild($style)
+    }
+  }
+  $style.textContent = style
 }
 
 if (typeof window !== 'undefined') {
