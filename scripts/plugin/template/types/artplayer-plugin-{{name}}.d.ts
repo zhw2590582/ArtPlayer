92ff80d285ac6897723ddf8fc7fcf823aae6b2d7
Harvey Zack <laozhaochaguan@gmail.com>
2025-09-06 15:13:23 +0800

    fix: add missing types export in package.json and correct interface syntax in TypeScript definition

--- scripts/plugin/template/types/artplayer-plugin-{{name}}.d.ts
+++ scripts/plugin/template/types/artplayer-plugin-{{name}}.d.ts
@@ -2,11 +2,11 @@ import type Artplayer from 'artplayer';
 
 interface Option {
     //
-};
+}
 
 interface Result {
     name: '{{export}}';
-};
+}
 
 declare const {{export}}: (option: Option) => (art: Artplayer) => Result;
 
