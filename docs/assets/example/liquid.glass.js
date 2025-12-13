bd6ce344209919d5e4cd66f0d35507be06d05a2b
Harvey Zhao <laozhaochaguan@gmail.com>
2025-12-13 17:19:40 +0800

    feat: add poster image and adjust liquid glass plugin styles

--- docs/assets/example/liquid.glass.js
+++ docs/assets/example/liquid.glass.js
@@ -4,6 +4,7 @@
 let art = new Artplayer({
   container: '.artplayer-app',
   url: '/assets/sample/video.mp4',
+  poster: '/assets/sample/poster.jpg',
   autoSize: true,
   pip: true,
   setting: true,
@@ -17,7 +18,7 @@ let art = new Artplayer({
     url: '/assets/sample/thumbnails.png',
     number: 60,
     column: 10,
-    scale: 0.85,
+    scale: 0.8,
   },
   plugins: [
     artplayerPluginLiquidGlass({
